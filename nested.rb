c45b75ca3154e88a77665678c93012ab1ec4eadf
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-28 18:08:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -3,11 +3,9 @@ def hopper
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
